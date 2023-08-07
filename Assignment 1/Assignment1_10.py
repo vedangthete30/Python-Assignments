24efb79e0cdf32ee9b1f3e9f4a3e54fdca389509
vedangthete30 <thete.vedang30@gmail.com>
2023-08-07 22:10:46 +0530

    Accept a name and return its length

---
+++ Assignment 1/Assignment1_10.py
@@ -0,0 +1,9 @@
+def LengthOfName(Name):
+    return len(Name)
+
+def main():
+    Name = input("Enter a name : ")
+    print ("The length of the entered name is ",LengthOfName (Name))
+
+if __name__ == "__main__":
+    main()
