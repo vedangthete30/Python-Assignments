860774c8c19eaa1d7ef6ef7b441c635674d0fdf7
vedangthete30 <thete.vedang30@gmail.com>
2023-08-15 18:59:00 +0530

    Created Arithmetic model

---
+++ Assignment 2/Arithmetic.py
@@ -0,0 +1,15 @@
+def Addition(num1, num2):
+    ans = num1 + num2
+    print("Addition of given numbers : ", ans)
+
+def Subtraction(num1, num2):
+    ans = num1 - num2
+    print("Subtraction of given numbers : ", ans)
+
+def Multiplication(num1, num2):
+    ans = num1 * num2
+    print("Multiplication of given numbers : ", ans)
+
+def Division(num1, num2):
+    ans = num1 / num2
+    print("Division of given numbers : ", ans)
