4067e5016618626370a1b5148b42f1e418ca1e8c
vedangthete30 <thete.vedang30@gmail.com>
2023-08-30 11:23:04 +0530

    Wap to calculate area and circumference followed by oop's concepts

--- Assignment 5/Assignment5_2.py
+++ Assignment 5/Assignment5_2.py
@@ -1,24 +1,26 @@
 class Circle:
     PI = 3.14
 
-    def __init__(self, radius):
-        self.Radius = radius
+    def __init__(self):
+        self.Radius = 0.0
+        self.Area = 0.0
+        self.Circumference = 0.0
         
     def Accept(self):
-        print("Radius given by the user is : ", self.Radius)
+        self.Radius = float(input("Enter radius of circle : "))
+                            
 
     def CalculateArea(self):
-        area = (Circle.PI) * (pow(self.Radius, 2))
-        print("Area of circle is : ", area)
+        self.Area = (Circle.PI) * (pow(self.Radius, 2))
+        print("Area of circle is : ", self.Area)
 
     def CalculateCircumference(self):
-        circumference = 2 * Circle.PI * self.Radius
-        print("Circumference is : ", circumference)
+        self.Circumference = 2 * Circle.PI * self.Radius
+        print("Circumference is : ", self.Circumference)
 
 
 def main():
-    r = float(input("Enter radius : "))
-    obj1 = Circle(r)
+    obj1 = Circle()
     obj1.Accept()
     obj1.CalculateArea()
     obj1.CalculateCircumference()
