3cef76840845a22efd3c9f3f5deb50e9e915aa1a
vedangthete30 <thete.vedang30@gmail.com>
2023-09-01 21:25:02 +0530

    wap to perform bank transactions

---
+++ Assignment 6/Assignment6_2.py
@@ -0,0 +1,49 @@
+class BankAccount:
+    ROI = 10.5  
+
+    def _init_(self):
+        self.Name = ""
+        self.Amount = 0.0
+        
+
+    def Display(self):
+        self.Name = input("Enter your name: ")
+        self.Amount = float(input("Enter initial amount in the account: "))
+        print(f"Name: {self.Name}, Amount: {self.Amount}")
+
+    def Deposit(self):
+        deposit_amount = float(input("Enter the amount to deposit: "))
+        self.Amount += deposit_amount
+        print(f"Deposited {deposit_amount} successfully.")
+        self.Display()
+
+    def Withdraw(self):
+        withdraw_amount = float(input("Enter the amount to withdraw: "))
+        if withdraw_amount <= self.Amount:
+            self.Amount -= withdraw_amount
+            print(f"Withdrew {withdraw_amount} successfully.")
+        else:
+            print("Insufficient balance.")
+        self.Display()
+
+    def CalculateInterest(self):
+        interest = (self.Amount * BankAccount.ROI) / 100
+        print(f"Interest calculated: {interest}")
+        self.Display()
+
+# Creating objects of BankAccount class
+account1 = BankAccount()
+account1.Display()
+
+account2 = BankAccount()
+account2.Display()
+
+
+account1.Deposit()
+account1.Withdraw()
+account1.CalculateInterest()
+
+
+account2.Deposit()
+account2.Withdraw()
+account2.CalculateInterest()
